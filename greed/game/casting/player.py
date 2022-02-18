b4db5afc371a588f7513d83a9e95deb90ecaca07
KabutoDrip <cammiller42003@gmail.com>
2022-02-18 16:39:11 -0500

    rock gem and player class creation

---
+++ greed/game/casting/player.py
@@ -0,0 +1,16 @@
+from game.casting.actor import Actor
+    
+class Player(Actor):
+
+    def __init__(self):
+        super().__init__()
+        self._message = ""
+        
+    def get_score(self):
+        ##gets the current score of the game
+        ()
+    def update_score(self):
+        ##updates the score of the 
+        ()
+    def give_score(self):
+        ()
