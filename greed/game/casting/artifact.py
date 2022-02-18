4a674e9a4fcb12047c0c4f7570bcc3704d31cb3f
Mary Goff <maryrgoff@gmail.com>
2022-02-18 12:45:40 -0700

    replaced game folder with rfk game folder.

--- greed/game/casting/artifact.py
+++ greed/game/casting/artifact.py
@@ -1,15 +1,31 @@
 from game.casting.actor import Actor
 
+
 class Artifact(Actor):
-    # responsibility of this class is to return info based on what item is collected
+    """
+    An item of cultural or historical interest. 
+    
+    The responsibility of an Artifact is to provide a message about itself.
+
+    Attributes:
+        _message (string): A short description about the artifact.
+    """
     def __init__(self):
         super().__init__()
-        self._collected = ""
-    def get_collected(self):
-    #gets the collected items type
-        return self._collected
-    def update_score(self, collected):
-        self._collected = collected
-    # updates score based on collected item
+        self._message = ""
+        
+    def get_message(self):
+        """Gets the artifact's message.
+        
+        Returns:
+            string: The message.
+        """
+        return self._message
     
-    
+    def set_message(self, message):
+        """Updates the message to the given one.
+        
+        Args:
+            message (string): The given message.
+        """
+        self._message = message
