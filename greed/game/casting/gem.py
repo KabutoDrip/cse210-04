0b6033f300ed1e593d295665f035653a3f3784e1
KK <91944125+k-kameta@users.noreply.github.com>
2022-02-20 14:25:48 +0900

    brief description added from the plan.

---
+++ greed/game/casting/gem.py
@@ -0,0 +1,12 @@
+'''
+Gem: 
+
+Replace Artifact with Rock(Actor) and Gem(Actor) and Player(Actor) # Make 3 inheriting classes.
+Gem: inherits actor. have point value attribute. have a getter for point value.
+    Player: inherit actor. have point score. have getter and setter for point score.
+    Rock: inherits actor. have point value attribute. have a getter for point value.
+
+Give rocks and gems velocity. # Do when called in __main__.py
+'''
+
+
