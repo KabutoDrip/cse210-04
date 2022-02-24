c55d7bd630f65e8d1d1eb945b5aafd35547c2f8c
KabutoDrip <cammiller42003@gmail.com>
2022-02-24 16:05:54 -0500

    updated player again

--- greed/game/casting/player.py
+++ greed/game/casting/player.py
@@ -2,21 +2,18 @@ from game.casting.actor import Actor
 from game.casting.cast import Cast    
 from game.casting.rock import Rock
 from game.casting.gem import Gem
+from game.directing.director import Director
 class Player(Actor):
 
     def __init__(self):
         super().__init__()
-        self._message = ""
-        self._score = 0
+        self._score = 1
         
     def get_score(self):
         # gets the current score of the game
         return self._score
-    def update_score(self):
+    def update_score(self, object_value):
         # updates the score of the 
-        score_mod = object.get_point()
-        self._score += score_mod
+        self._score += object_value
         return self._score
-    def give_score(self,score):
-        self._score = score
-        return self._score
+   
